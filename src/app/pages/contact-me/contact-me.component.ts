afc8edc86f65c7b70168de98eefd138f380cb70f
Your Username <parhambarati32@gmail.com>
2023-09-06 09:52:14 -0400

    add contact me and colors

--- src/app/pages/contact-me/contact-me.component.ts
+++ src/app/pages/contact-me/contact-me.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, OnInit } from '@angular/core';
+import { fadeIn, fadeOut } from '../../animation/fade';
 
 @Component({
   selector: 'app-contact-me',
   templateUrl: './contact-me.component.html',
   styleUrls: ['./contact-me.component.scss']
 })
-export class ContactMeComponent {
+export class ContactMeComponent implements OnInit {
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+  ngOnInit(): void {
+    this.calculateAndSetHeight();
+  }
+
+  calculateAndSetHeight() {
+    const welcomeElement = this.elementRef.nativeElement.querySelector('.contactMe');
+    if (welcomeElement) {
+      const windowHeight = window.innerHeight;
+      this.renderer.setStyle(welcomeElement, 'height', `${windowHeight}px`);
+    }
+  }
 
 }
